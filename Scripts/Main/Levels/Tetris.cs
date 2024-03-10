7d81568cf7f725d63c2084bdc555238383ed09d4
Gcreach <gcreach97@gmail.com>
2024-03-09 19:21:39 -0700

    changes to level

---
+++ Scripts/Main/Levels/Tetris.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class Tetris : Node2D
+{
+
+	[Export] public Label TurnLabel;
+
+	private string turnOn = "";
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.RandRange(150, 250);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void UpdateLabels() {
+		if(turnOn == "blue") {
+			TurnLabel.Text = "Blue";
+		}
+		else if (turnOn == "Red") {
+			TurnLabel.Text = "Red";
+		 }
+		 else if (turnOn == "Yellow") {
+			TurnLabel.Text = "Yellow";
+		  }
+	}
+
+	
+}
