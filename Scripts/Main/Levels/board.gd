877ee925c47f940ba2097d7659359044d0ab1695
Gcreach <gcreach97@gmail.com>
2024-03-16 18:41:33 -0700

    changes to baord

--- Scripts/Main/Levels/board.gd
+++ Scripts/Main/Levels/board.gd
@@ -3,13 +3,15 @@ extends Node
 class_name Board
 
 signal parasite_locked
-@onready var panel_container = $"../PanelContainer"
+signal game_over
 
 var next_parasite
 const ROW_COUNT = 15
 const COLUMN_COUNT = 10
 
 var parasites: Array[parasite] = []
+
+@onready var line_scene = preload("res://Scenes/line.tscn")
 
 @export var parasite_scene: PackedScene
 
@@ -21,69 +23,64 @@ func spawn_parasite(type: Shared.parasite, is_next_piece, spawn_position):
 	Parasite.is_next_piece = is_next_piece
 	
 	if is_next_piece == false:
-		var other_pieces = get
+		
 		Parasite.position = parasite_data.spawn_position
 		Parasite.other_parasites = parasites
 		Parasite.lock_parasite.connect(on_parasite_locked)
-		add_child(Parasite)
-	else: 
-		Parasite.scale = Vector2(0.5, 0.5)
-		#TODO: Fix bug below
-		panel_container.add_child(Parasite)
-		Parasite.set_position(spawn_position)
-		next_parasite = Parasite
-		
+		add_child(Parasite)		
 		
 func on_parasite_locked(Parasite: parasite):
 	next_parasite.queue_free()
 	parasites.append(Parasite)
+	add_parasite_to_line(Parasite)
+	remove_full_lines()
 	parasite_locked.emit()
-	clear_lines()
+	check_game_over()
 	
-func clear_lines():
-	var board_pieces = fill_board_pieces()
-	clear_board_pieces(board_pieces)
 	
-func fill_board_pieces():
-	var board_pieces = []
 	
-	for i in ROW_COUNT:
-		board_pieces.append([])
+func check_game_over():
+		for piece in get_all_pieces():
+			var y_location = piece.global_position.y
+			if y_location == -456:
+				game_over.emit()
+
+func add_parasite_to_line(Parasite: parasite):
+	var parasite_pieces = Parasite.get_children().filter(func (c): return c is Piece)
+	
+	for piece in parasite_pieces:
+		var y_position = piece.global_position.y
+		var does_line_for_piece_exists = false
 		
-	for Parasite in parasites:
-		var parasite_pieces = Parasite.get_children().filter(func (c): return c is Piece)
-		for piece in parasite_pieces:
+		for line in get_lines():
 			
-			var row = (piece.global_position.y + piece.get_size().y / 2) / piece.get_size().y + ROW_COUNT / 2
-			#TODO: Fix bug below
-			board_pieces[row - 1].append(piece)
-	return board_pieces
-	
-func clear_board_pieces(board_pieces):
-	var i = ROW_COUNT
+			if line.global_position.y == y_position:
+				piece.reparent(line)
+				does_line_for_piece_exists = true
 		
-	while i > 0:
-		var row_to_anaylze = board_pieces[i - 1]
-			
-		if row_to_anaylze.size() == COLUMN_COUNT:
-			clear_row(row_to_anaylze)
-			board_pieces[i - 1].clear()
-			move_all_row_pieces_down(board_pieces, i)
-		i -= 1
-			
-func clear_row(row):
-	for piece in row:
-		piece.queue_free()
-		
-func move_all_row_pieces_down(board_pieces, cleared_row_number):
-	for i in range(cleared_row_number -1, 1, -1):
-		var row_to_move = board_pieces[i -1]
-		if row_to_move.size() == 0:
-			return false
-			
-		for piece in row_to_move:
-			piece.position.y += piece.get_size().y
-			board_pieces[cleared_row_number - 1].append(piece)
-		board_pieces[i -1].clear()
-				
+		if !does_line_for_piece_exists:
+			var piece_line = line_scene.instantiate() as Line
+			piece_line.global_position = Vector2(0, y_position)
+			add_child(piece_line)
+			piece.reparent(piece_line)
+
+func get_lines():
+	return get_children().filter(func (c): return c is Line)
+
+func remove_full_lines():
+	for line in get_lines():
+		if line.is_line_full(COLUMN_COUNT):
+			move_lines_down(line.global_position.y)
+			line.free()
+
+func move_lines_down(y_position):
+	for line in get_lines():
+		if line.global_position.y < y_position:
+			line.global_position.y += 48
+
+func get_all_pieces():
+	var pieces = []
+	for line in get_lines():
+		pieces.append_array(line.get_children())
+	return pieces
 
